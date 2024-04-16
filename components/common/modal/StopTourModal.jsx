4fbf3d85688199720a1d50bbf9da1d61f1642197
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-04-17 00:50:58 +0700

    [refactor] remove unused icons

--- components/common/modal/StopTourModal.jsx
+++ components/common/modal/StopTourModal.jsx
@@ -1,5 +1,6 @@
 import { Modal, Text, Pressable, View } from 'react-native';
 import styles from './modal.style';
+import { COLORS } from '../../../constants';
 
 const StopTourModal = ({ isVisible, setVisble, handleConfirm }) => {
     return (
@@ -12,20 +13,20 @@ const StopTourModal = ({ isVisible, setVisble, handleConfirm }) => {
             }}>
             <View style={styles.centeredView}>
                 <View style={styles.modalView}>
-                    <Text style={styles.headerText}>Confirm Stop</Text>
+                    <Text style={[styles.headerText, {color: COLORS.primary}]}>Confirm Stop</Text>
                     <Text style={styles.contentText}>Are you sure to stop tour guide ? </Text>
                     <View style={styles.btnWrapper}>
                         <Pressable
                                 style={[styles.button, styles.cancelBtn]}
                                 onPress={() => setVisble(false)}
                             >
-                                <Text style={styles.btnText}>Cancel</Text>
+                                <Text style={[styles.btnText, {color:COLORS.red}]}>Cancel</Text>
                             </Pressable>
                         <Pressable
-                            style={[styles.button, styles.confirmBtn]}
+                            style={[styles.button, {backgroundColor: COLORS.red}]}
                             onPress={handleConfirm}
                         >
-                            <Text style={styles.btnText}>Confirm</Text>
+                            <Text style={[styles.btnText, {color: COLORS.lightWhite}]}>Stop Tour</Text>
                         </Pressable>
                     </View>
                 </View>
--- constants/icons.js
+++ constants/icons.js
@@ -1,18 +1,14 @@
 import heart from '../assets/icons/heart.png';
-import menu from '../assets/icons/menu.png';
 import search from '../assets/icons/search.png';
 import left from '../assets/icons/left.png';
 import heartOutline from '../assets/icons/heart-ol.png';
 import share from '../assets/icons/share.png';
 import desLocation from '../assets/icons/des-location.png';
-import chevronLeft from '../assets/icons/chevron-left.png';
-import chevronRight from '../assets/icons/chevron-right.png';
 import locationIcon from '../assets/icons/location-ol.png';
 import send from '../assets/icons/send.png';
 import hidePassword from '../assets/icons/hide-password.png';
 import google from '../assets/icons/company/google.png';
 import facebook from '../assets/icons/company/facebook.png';
-import fastForward from '../assets/icons/fast-forward.png';
 import botLocation from '../assets/icons/bot-location.png';
 import pickupLocation from '../assets/icons/pickup-location.png';
 import stop from '../assets/icons/stop-button.png';
@@ -26,14 +22,11 @@ import profile from '../assets/icons/tab-navigator/profile.png';
 
 export default {
   heart,
-  menu,
   search,
   left,
   heartOutline,
   share,
   desLocation,
-  chevronLeft,
-  chevronRight,
   home,
   history,
   map,
@@ -43,7 +36,6 @@ export default {
   hidePassword,
   google,
   facebook,
-  fastForward,
   botLocation,
   pickupLocation,
   stop,
